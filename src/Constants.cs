9bb44549b8789174bd88d1c6d087ff28ffdbbb95
Jake Soenneker <jake@soenneker.com>
2024-12-28 12:15:40 -0600

    constants mod

--- src/Constants.cs
+++ src/Constants.cs
@@ -2,6 +2,6 @@ namespace Soenneker.Runners.Whisper.CTranslate;
 
 public static class Constants
 {
-    public const string FileName = "whisper-ctranslate2.exe";
+    public const string FileName = "whisper_ctranslate2.exe";
     public const string Library = "Soenneker.Libraries.Whisper.CTranslate";
 }
